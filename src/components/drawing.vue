289d98e5bacfb394ae5b78353074e34f06131d49
Hendrik Lösch <loeschhendrik@googlemail.com>
2019-10-15 14:35:35 +0200

    reference data properties (interpolation)

--- src/components/drawing.vue
+++ src/components/drawing.vue
@@ -1,12 +1,27 @@
 <template>
   <div id="drawing">
-      Drawing
+      <div>{{drawing.Datum}}</div>
   </div>
 </template>
 
 <script>
 export default {
-    name: "drawing"
+    name: "drawing",
+    data() {
+      return {
+        drawing: {
+          "Datum": "2019-05-10T00:00:00",
+          "ez1": 8,
+          "ez2": 3,
+          "Id": 71,
+          "z1": 29,
+          "z2": 15,
+          "z3": 7,
+          "z4": 19,
+          "z5": 5
+      }
+      }
+    },
 }
 </script>
 
