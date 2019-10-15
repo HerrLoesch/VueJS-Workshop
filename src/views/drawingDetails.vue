0d4ee60d46d432adc466a8cdc03326b4eb8873d9
Hendrik Lösch <loeschhendrik@googlemail.com>
2019-10-15 17:54:06 +0200

    Added drawing details

---
+++ src/views/drawingDetails.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-container id="drawingDetails">
+    <v-card>
+      <v-card-text>
+        <drawing :value="drawing"></drawing>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn icon @click="goBack">
+          <v-icon>mdi-arrow-left</v-icon>
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-container>
+</template>
+
+<script>
+import drawing from "@/components/drawing";
+export default {
+  name: "drawingDetails",
+  components: {
+    drawing
+  },
+  mounted() {
+
+  },
+  methods: {
+    goBack() {}
+  },
+  data() {
+    return {
+      drawing: {}
+    };
+  }
+};
+</script>
