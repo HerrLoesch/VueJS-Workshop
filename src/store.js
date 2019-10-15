c556e0e034cde00adf5b0a630b314e88fe11acd6
Hendrik Lösch <loeschhendrik@googlemail.com>
2019-10-15 18:16:03 +0200

    added store

--- src/store.js
+++ src/store.js
@@ -1,16 +1,34 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue"
+import Vuex from "vuex"
+import drawingService from "./services/drawingService"
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+    isLoading: false,
+    drawings: []
   },
   mutations: {
-
+    setDrawings(state, drawings) {
+      state.drawings = drawings
+    },
+    startLoading(state) {
+      state.isLoading = true
+    },
+    stopLoading(state) {
+      state.isLoading = false
+    }
   },
   actions: {
+    async initialize({ commit }) {
 
+      commit("startLoading")
+
+      var drawings = await drawingService.getDrawings()
+      commit("setDrawings", drawings)
+
+      commit("stopLoading")
+    }
   }
 })
